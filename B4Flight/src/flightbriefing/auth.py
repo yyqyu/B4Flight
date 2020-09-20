a3d2308d8d6a7dd92543589a471c10292f6889d9
Andrew Retallack <artech@live.co.za>
2020-09-20 11:37:06 +0200

    Password Recovery Functionality

--- B4Flight/src/flightbriefing/auth.py
+++ B4Flight/src/flightbriefing/auth.py
@@ -235,7 +235,7 @@ def register():
             new_user = User()
             new_user.Username = username
             new_user.Email = email
-            new_user.Password = passwd
+            new_user.set_password(passwd)
             new_user.Firstname = firstname
             new_user.Lastname = lastname
             
@@ -376,6 +376,142 @@ def login():
         flash(error_msg)
         
     return render_template('auth/login.html')
+
+
+@bp.route('/forgotpass', methods=('GET', 'POST'))
+def forgotpass():
+    """Show html page allowing user to request a password reset.
+    Once the request is submitted, validate username and send e-mail to user with reset link
+    
+    """
+    # Process the form returned
+    if request.method == 'POST':
+        
+        username = request.form['username']
+
+        # Validate username supplied.  Client-side validation is performed - this is a backup to that
+        if not username:
+            flash('Please enter a username.')
+
+        # Username supplied
+        else:
+
+            sess = sqa_session() #SQLAlchemy Session
+
+            # Retrieve a user if one exists
+            forgot_user = sess.query(User).filter(User.Username == username).first()
+            
+            # If a user exists, then send them an e-mail with a pwd recovery link
+            if not forgot_user is None:
+
+                # Create a password recovery token for the user
+                recovery_token = forgot_user.create_recovery_token()
+                
+                # Create an HTML and a text e-mail body including the authentication token
+                msg_html = render_template('emails/recover_pass.html', token=recovery_token, user_fname=forgot_user.Firstname)
+                msg_txt = render_template('emails/recover_pass.txt', token=recovery_token, user_fname=forgot_user.Firstname)
+                
+                # Create the email addresses in python's Address format
+                user_fullname = forgot_user.Firstname + ' ' + forgot_user.Lastname
+                mail_to = Address(display_name = user_fullname.rstrip(), addr_spec = forgot_user.Email)
+                mail_from = Address(display_name = current_app.config['EMAIL_ADMIN_NAME'], addr_spec = current_app.config['EMAIL_ADMIN_ADDRESS'])
+                
+                # Send the e-mail - BCC in the administrator
+                was_mail_sent = helpers.send_mail(mail_from, mail_to,'Recover your B4Flight password', msg_txt, msg_html, recipients_bcc=mail_from)
+                
+                # If the mail wasn't sent, warn the user
+                if was_mail_sent == False:
+                    flash('We tried to send you a password reset mail but encountered a problem.  Please try again or contact us.', 'error')
+                
+                # Otherwise show a message and redirect to login page
+                else:
+                    flash("PASSWORD RESET: We've sent you an e-mail with password recovery instructions (assuming the username you entered exists).", "success")
+                    return redirect(url_for('auth.login'))
+            
+            # Even though the user doesn't exist, we display the same message and redirect to login page
+            else:
+                flash("PASSWORD RESET: We've sent you an e-mail with password recovery instructions (assuming the username you entered exists).", "success")
+                return redirect(url_for('auth.login'))
+
+    # Show the html page to reset the password
+    return render_template('auth/forgotpass.html')
+
+
+@bp.route('/passreset/<token>', methods=('GET', 'POST'))
+def passreset(token):
+    """This function is run when the user clicks the password reset link in the e-mail
+    Validate the link the user clicked and display the password reset screen.
+    If link is not valid, show an error and redirect to login screen
+    """
+
+    # A POST request means the user has submitted the form with a new password
+    if request.method == 'POST':
+        
+        username = request.form['username']
+        passwd = request.form['password']
+        passwdchk = request.form['passwordcheck']
+        
+        error_msg = None
+        
+        # Client-side validation checks all these. These validations are a backup
+        if not username:
+            error_msg = 'Please enter a username.'
+        elif not passwd:
+            error_msg = 'Please enter a password.'
+        elif not passwdchk:
+            error_msg = 'Please enter both passwords.'
+
+        elif len(passwd) < 8:
+            error_msg = 'Please enter a password of at least 8 characters.'
+
+        elif passwd != passwdchk:
+            error_msg = 'You did not re-enter your password correctly.'
+        
+        if error_msg:
+            flash(error_msg, 'error')
+            return render_template('auth/resetpass.html',username=username)
+        
+        
+        # Form was validated - now reset the password
+        
+        sess = sqa_session() #SQLAlchemy Session
+
+        # Retrieve the user
+        forgot_user = sess.query(User).filter(User.Username == username).first()
+        # Update password
+        forgot_user.set_password(passwd)
+        # Save to DB
+        sess.commit()
+        
+        flash('Your password has been reset.  Please login', 'success')
+        return redirect(url_for('auth.login'))
+    
+    # If not a POST then must be a GET - therefore this is the first click on the link
+    else:
+        # If there is no token, show an error msg and redirect to login form
+        if token is None:
+            flash('The poassword recovery link you clicked on was not valid.', 'error')
+            return redirect(url_for('auth.login'))
+
+        # Validate the token, and display password reset screen
+        usr = User.validate_recovery_token(token)
+    
+        # if function returns None, it means link is not valid
+        if usr is None:
+            # Flash error message and redirect to the login page 
+            flash('The poassword recovery link you clicked on was not valid. Please check you copied it correctly.', 'error')
+            return redirect(url_for('auth.login'))
+        
+        # If function returns "-1" the link has expired
+        elif usr == -1:
+            # Flash error message and redirect to the login page
+            flash('The poassword recovery link you clicked on has expired. Links are only valid for 24 hrs', 'error')
+            return redirect(url_for('auth.login'))
+        
+        # Token is valid - show the password reset form 
+        else:
+            return render_template('auth/resetpass.html',username=usr.Username)
+
 
 
 @bp.route('/logout')
