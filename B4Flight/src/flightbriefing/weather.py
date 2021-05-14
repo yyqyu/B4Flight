91572a7323944ec08d93225d628f9642e6b7a0b8
Andrew Retallack <artech@live.co.za>
2021-05-14 11:11:02 +0200

    Added API to retrieve METARS

--- B4Flight/src/flightbriefing/weather.py
+++ B4Flight/src/flightbriefing/weather.py
@@ -13,13 +13,18 @@ from datetime import datetime, timedelta
 
 import requests
 from bs4 import BeautifulSoup
+import json
 
-from flask import current_app
+from flask import (
+    Blueprint, redirect, render_template, request, session, url_for, current_app, flash, abort
+)
+#from flask import current_app
 
 from .data_handling import sqa_session
 from . import helpers
 from .db import NavPoint
 
+bp = Blueprint('weather', __name__, url_prefix='/weather')
 
 
 def calc_metar_taf_date(day, hr, mn=0):
@@ -284,7 +289,7 @@ def generate_sigmet_geojson(sigair_met_list):
 
 
 
-def read_metar_ZA(metar_url):
+def read_metar_ZA(metar_url, date_as_ISO_text=False):
     """ Function that webscrapes METAR data from specified URL, 
         returning a list of METAR dictionary items for further processing
         
@@ -293,6 +298,8 @@ def read_metar_ZA(metar_url):
     ----------
     metar_url: string
         URL from which to scrape the METAR data
+    date_as_ISO_text: boolean, optional
+        Return the Metar Date/Time as an ISO text string (allows use in JSON)
     
     Returns
     -------
@@ -438,7 +445,9 @@ def read_metar_ZA(metar_url):
                 qnh = tmp.group('qnh')
             except:
                 current_app.logger.error(f"Error passing METAR QNH: {met_string}")
-
+        
+        if date_as_ISO_text == True:
+            met_date = datetime.isoformat(met_date)
         
         met_dict = {'aerodrome': aerodrome , 'coords': (aero_point.Longitude, aero_point.Latitude), 
                     'has_no_data': False , 'is_speci': is_speci, 'time': met_date, 
@@ -730,3 +739,65 @@ def generate_taf_geojson(taf_list):
                                                                  'text': this_taf['body']}))
 
     return taf_features
+
+
+@bp.route('/get_metar_za', methods=('POST','GET'))
+def get_metar_za():
+    """API to return Metars in a JSON format for ZA aerodromes
+    Returns JSON structure containing Metar data
+    
+    Expects: No data: returns all aerodromes
+            {aerodrome: ICAO} - returns for specified aerodrome
+    
+    Returns: json array of objects:
+            aerodrome: ICAO code
+            has_no_data: boolean
+            is_speci: boolean
+            time: date and time of the METAR
+            wind: tuple containing (direction, strength, gusting, is_variable).  Direction of -1 means variable
+            temperature: temp in degrees centigrade
+            dew_point: dewpoint temp in degrees centigrade (integer, so M01 is shown as -01)
+            QNH: QNH in hPa
+            body: full body of the METAR
+            coords: co-ord pair for the aerodrome - LONG, LAT in decimal degrees
+            
+            If no data found, returns: {'error': 'No data found'}
+    
+    """
+
+    metars = read_metar_ZA(current_app.config['WEATHER_METAR_URL_ZA'], date_as_ISO_text=True)
+    
+
+    filter_aerodrome = None
+    
+    if request.method == 'GET':
+        # Get the aerodrome to filter by
+        try:
+            filter_aerodrome = request.args.get('aerodrome')
+        except:
+            filter_aerodrome = None
+    
+    # Process the request's data
+    if request.method == 'POST':
+        # Get data in JSON format
+        req_data = request.get_json()
+
+        # If there is request data then process it
+        if req_data:
+            # Get the aerodrome to filter by
+            try:
+                filter_aerodrome = req_data.get('aerodrome')
+            except:
+                filter_aerodrome = None
+        
+    #If we have an aerodrome to filter by
+    if filter_aerodrome:    
+        metar_filter = filter(lambda met: met['aerodrome'] == filter_aerodrome, metars)
+        metars = list(metar_filter)
+        
+    #If nothing returned, then error
+    if len(metars) == 0: 
+        return json.dumps({'error' : 'No data found'})
+            
+    # Return the metars
+    return json.dumps(metars)
